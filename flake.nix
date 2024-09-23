201ccc9b93e4b351566fee6518ff59c95e008e67
prdsrm <mail@prdsrm.pro>
2024-09-23 17:41:24 +0000

    Added Nix environment to simplify developper experience

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+         config.allowUnfree = true;
+      };
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          # Golang & build tools
+          gnumake
+          go
+          golangci-lint
+          gopls
+          delve
+        ];
+      };
+    });
+}
