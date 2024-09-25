55e4102978bd65e811c161280520c66adf088e2f
prdsrm <mail@prdsrm.pro>
2024-09-25 08:41:55 +0000

    Added helpers to search through dialogs

---
+++ dialogs/search.go
@@ -0,0 +1,47 @@
+package dialogs
+
+import (
+	"cmp"
+	"context"
+	"slices"
+
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/telegram/query"
+	"github.com/gotd/td/telegram/query/dialogs"
+	"github.com/gotd/td/tg"
+	"github.com/prdsrm/std/utils"
+)
+
+func GetDialogs(ctx context.Context, client *telegram.Client) ([]dialogs.Elem, error) {
+	builder := query.GetDialogs(client.API())
+	dialogs, err := builder.Collect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return dialogs, nil
+}
+
+func SortPeersInDialogs(elems []dialogs.Elem) []tg.PeerClass {
+	var peers []tg.PeerClass
+	for _, elem := range elems {
+		peers = append(peers, elem.Dialog.GetPeer())
+	}
+	slices.SortFunc(peers, func(firstPeerClass tg.PeerClass, secondPeerClass tg.PeerClass) int {
+		id1 := utils.GetIDFromPeerClass(firstPeerClass)
+		id2 := utils.GetIDFromPeerClass(secondPeerClass)
+		return cmp.Compare(id1, id2)
+	})
+	return peers
+}
+
+func compare(peerClass tg.PeerClass, id int64) int {
+	return cmp.Compare(utils.GetIDFromPeerClass(peerClass), id)
+}
+
+func SearchTroughPeers(peers []tg.PeerClass, id int64) (*tg.PeerClass, bool) {
+	index, found := slices.BinarySearchFunc(peers, id, compare)
+	if found {
+		return &peers[index], true
+	}
+	return nil, false
+}
